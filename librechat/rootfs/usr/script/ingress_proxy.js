23a58483364ac759edbd92871060ae9c246df742
Bobby Hirst <166758526+BobbyQuantum@users.noreply.github.com>
2024-05-12 19:25:28 +0100

    feat: Add ingress proxy

---
+++ librechat/rootfs/usr/script/ingress_proxy.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const httpProxy = require("http-proxy");
+
+// Create a proxy server with custom application logic
+const proxy = httpProxy.createProxyServer({});
+
+// Set up express
+const app = express();
+
+app.use("/api/hassio_ingress/:token", (req, res) => {
+  // Modify the incoming request URL by stripping off unwanted parts
+  let forwardedUrl = req.url.split("/").slice(4).join("/");
+
+  // Your target where you want to send the cleaned URL
+  const targetUrl = `http://helix.local:8123/${forwardedUrl}`;
+
+  // Perform the proxying
+  proxy.web(req, res, { target: targetUrl });
+});
+
+// Specify the port to run on
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Proxy server running on port ${PORT}`);
+});
